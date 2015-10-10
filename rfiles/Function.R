f9392faf8be07db0ea9f003200d460f803ce3328
unknown <hitwzh@gmail.com>
2015-10-10 23:05:53 +0800

    get clusters and trips

--- rfiles/Function.R
+++ rfiles/Function.R
@@ -36,3 +36,116 @@ get.raw.traj<-function(dir,lon1,lat1,lon2,lat2){
   t=t[,sog:=round(sog*3600/1000/1852*10)]
   return(t)
 }
+
+#get clusters on different levelwhich is controlled by 
+#e:the distance of the density circle
+#MinPts:min points required at each circle when the center point is within the cluster
+
+getclusters<-function(stops,e,MinPts){
+  
+  model <- dbscan(as.matrix(stops[,list(lon,lat)]),eps=e,minPts=MinPts)  # 50000 for archor places5
+  stops=cbind(stops,cluster=model$cluster)
+  return(stops)
+  
+}
+
+drawClst<-function(dt){
+  library(ggplot2)
+  p <-ggplot(dt,aes(x=lon,y=lat))
+  # p=getMap(x[cluster>0],5)
+  p=p+geom_point(data=dt,size=1, aes(colour=factor(cluster)),shape=3)
+  p=p+geom_text(data=dt[,.SD[1],by=cluster],size=3,col='black',aes(x=lon+0.05,y=lat+0.05,label=cluster))
+  p
+}
+#break the trajectores with start and end clusters
+#the trip does not include points of start and end clusters
+#this function may be changed to set two or three level original and destination trips
+
+get.trips<-function(dt){
+  
+  dt[,cluster.start:=0];dt[,cluster.end:=0];dt[,trip:=0]
+  dt$cluster.start=as.integer(dt$cluster.start);dt$cluster.end=as.integer(dt$cluster.end)
+  #get mmsis with 2 points at least
+  mmsis=unique(dt[,.N,by=mmsi][N>=2]$mmsi);mmsis.len=length(mmsis);
+  x=dt[mmsi==0]
+  print(mmsis.len)
+  print(Sys.time())
+  for (i in(1: mmsis.len)){
+    if(i%%100==0){
+      print(i)
+    }
+    aship=dt[mmsi==mmsis[i]];setkey(aship,time);len=nrow(aship);
+    tmp1=aship[1:(len-1)]$cluster
+    tmp2=aship[2:len]$cluster
+    tmp=as.data.table(tmp2-tmp1)
+    idx.n=tmp[V1<0,which=TRUE]
+    idx.p=tmp[V1>0,which=TRUE]
+    idx=as.data.table(c(1,len,idx.n,idx.p))
+    idx=unique(idx)
+    setkey(idx,V1)
+    l=nrow(idx)
+    
+    for (j in (1:(l-1))){
+      
+      #first row in index.
+      if(j==1){
+        #idx[1] means the first number(the index of dt) in idx
+        range=idx[j]$V1:idx[j+1]$V1
+        sc=aship[idx[j]$V1]$cluster
+        aship[range,trip:=j]
+      }else{
+        range=(idx[j]$V1+1):(idx[j+1]$V1)
+        sc=aship[idx[j]$V1]$cluster
+        
+      }
+      aship[range,trip:=j]
+      
+      #last row with different cluster
+      if(j==(l-1)){
+        ec=aship[idx[j+1]$V1]$cluster
+      }else{
+        ec=aship[idx[j+1]$V1+1]$cluster
+      }
+      
+      aship[range,cluster.start:=sc]
+      aship[range,cluster.end:=ec]
+      #for cluster>0 its start and end cluster is the same one
+      #before is mainly for cluster==0
+      aship[cluster>0,cluster.start:=cluster]
+      aship[cluster>0,cluster.end:=cluster]
+      
+    }
+    
+    x=rbind(x,aship)
+  }
+  setkey(x,mmsi,time)
+  return(x)
+  print(Sys.time())
+}
+
+plot.route<-function(dt){
+  dev.new()
+  #p<-getMap(dt,8)
+  p=ggplot()
+  p=p+geom_point(data=dt,aes(x=lon,y=lat,color=factor(trip)))
+  p=p+geom_path(data=dt,aes(x=lon,y=lat,color=factor(trip)))
+  p
+  
+}
+
+getMap<-function(dt,zoomsize){
+  library('ggmap')
+ 
+  lon=dt$lon
+  lat=dt$lat
+  
+  centerX=0.5*(max(lon)+min(lon))
+  centerY=0.5*(max(lat)+min(lat))
+  
+  p<-ggmap(get_map(location=c(centerX,centerY),zoom=zoomsize,source='google'))
+  
+  return(p)
+  
+  
+  
+}
--- rfiles/Main.R
+++ rfiles/Main.R
@@ -1,4 +1,7 @@
 library(data.table)
+library(dplyr)
+library(dbscan)
+library(igraph)
 #----------prepare data-------------------------
 shipfile='D:/share/Rprojects/inputs/ships.txt';
 ships=getships(shipfile);dim(ships);head(ships);setkey(ships,mmsi);
@@ -6,4 +9,37 @@ container.mmsis=ships[type_en=='Container',list(mmsi=as.numeric(mmsi))]
 dir='D:/share/AIS/AIS_chuanxun_201409/csvdata'
 t=get.raw.traj(dir,108,18,126,42)
 t.container=data.table(inner_join(t,container.mmsis,by='mmsi'));head(t.container);setkey(t.container,mmsi,time)
-#-----------get clusters------------------------
+t.container=t.container[sog<250];
+t.points=t.container[,pid:=1:nrow(t.container)]
+t.grids=t.points[,level3lon:=floor(lon*1000)]
+t.grids=t.grids[,level3lat:=floor(lat*1000)];setkey(t.grids,level3lon,level3lat)
+t.grids=t.points[,level2lon:=floor(lon*100)]
+t.grids=t.grids[,level2lat:=floor(lat*100)];setkey(t.grids,level2lon,level2lat)
+t.grids=t.points[,level1lon:=floor(lon*10)]
+t.grids=t.grids[,level1lat:=floor(lat*10)];setkey(t.grids,level1lon,level1lat)
+#mean is much faster than median
+
+#-----------get clusters for each points---------
+#input trajectories and out put trajectories with cluster number
+t.grids.level3=t.grids[sog<10,list(lon=median(lon),lat=median(lat)),by=list(level3lon,level3lat)]
+dim(t.grids.level3)
+t.cluster=getclusters(t.grids.level3,0.25,20);t.cluster[,.N,by=cluster]
+#each point with a cluster, cluster of points not in t.cluster is set to 0 as well.
+#now the class is at a quite high level.
+t.grids.cluster=left_join(t.grids,t.cluster[cluster>0,list(cluster),by=c('level3lon','level3lat')])
+t.grids.cluster=t.grids.cluster[is.na(cluster),cluster:=0];setkey(t.grids.cluster,mmsi,time)
+drawClst(t.cluster[cluster>0])
+write.csv(t.cluster,file='D:/share/AIS/AIS_chuanxun_201409/containers/cluster3.csv',sep=',')
+
+#------------beark trajectories of each ship with an start and end cluster---------------
+# pay attention to some segments which may be on the way to an port 
+
+
+t.trips=get.trips(t.grids.cluster)
+
+t.route=t.trips[cluster.start==14&cluster.end==5];dim(t.route);head(t.route)
+write.csv(t.route,file='D:/share/AIS/AIS_chuanxun_201409/containers/GaoXiongToXiaMen.csv',sep=',')
+trip.timespan=t.route[,list(timespan=max(.SD$time)-min(.SD$time)),by=list(mmsi,trip)]
+plot.route(t.trips[cluster==14|cluster==5])
+
+
